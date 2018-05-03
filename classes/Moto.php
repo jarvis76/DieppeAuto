148ebb6662ea8fd2918e0a4f08ac8df3a8cc04e4
christopher <christopher@sfr.fr>
2018-05-03 11:43:27 +0200

    Ecriture classe Moto avec implémentation interface Consommation

---
+++ classes/Moto.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 03/05/2018
+ * Time: 11:40
+ */
+
+class Moto implements Consommation
+{
+    public function polluer()
+    {
+        // TODO: Implement polluer() method.
+    }
+}
