286ff3aae1dbca73f3360c4b02df106a717b15a3
christopher <christopher@sfr.fr>
2018-05-03 11:43:43 +0200

    Classe abstraite Voiture

---
+++ classes/Voiture.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 03/05/2018
+ * Time: 10:01
+ */
+
+abstract class Voiture
+{
+    protected $couleurDominante;
+    protected $masse;
+    protected $marque;
+    protected $modele;
+    protected $vitesse;
+
+
+    public function __construct($mark, $mode1)
+    {
+        $this->marque = $mark;
+        $this->modele = $model;
+    }
+
+    abstract public function distanceFreinage();
+
+
+}
