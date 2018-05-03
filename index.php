0389aecb7572d39a8adce50367812fbaf7d12781
christopher <christopher@sfr.fr>
2018-05-03 10:00:45 +0200

    autoloader et fuseau horraire

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+
+<h1> Bienvenue sur DieppeAuto </h1>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title> DieppeAuto </title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a> </li>
+
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
+
+
+
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,4 +32,9 @@
  * Time: 09:49
  */
 
+
+date_default_timezone_set("Europe/Paris");
 require_once "./functions/classAutoLoader.php";
+spl_autoload_register('classAutoloader');
+
+
